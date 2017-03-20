6ce85b9068328f27565d85500fce17d46fa45c6a
horchi <vdr@jwendel.de>
2017-03-20 20:38:54 +0100

    fix

--- config.c
+++ config.c
@@ -21,7 +21,7 @@ cOsd2WebConfig config;
 cOsd2WebConfig::cOsd2WebConfig()
 {
    webPort = 4444;
-   clientOsdTimeout = 12;
+   clientOsdTimeout = 120;
    epgImagePath = 0;
    logoNotLower = no;
    logoById = no;
