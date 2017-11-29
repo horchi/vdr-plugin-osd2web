0fc90d61b8998e52faca2559a8d06a701dd16e70
horchi <vdr@jwendel.de>
2017-11-29 08:15:43 +0100

    dia show development

--- client/src/components/DiaShow.vue
+++ client/src/components/DiaShow.vue
@@ -1,23 +1,16 @@
 <template>
   <div >
     <div class="diashow card mt-1">
-      <div v-show="diashow.title" style="height: 100%;">
-        <div class="row">
-          <h3 class="card-title col-12 col-md-9 titletxt">{{diashow.title}}</h3>
-        </div>
-        <div class="clearfix">
-          <div :id="'diaImages' + diashow.filename" class="img-fluid float-right img-thumbnail carousel slide" data-ride="carousel" data-interval="5000">
-            <div class="carousel-inner dia-image" role="listbox">
-              <div v-for="(img, n) in diashow.images" class="carousel-item" :class="{'active':n==0}">
-                <img class="d-block" :src="diashow.filename" style="width: 100%;" alt="">
-              </div>
-            </div>
+      <div class="clearfix">
+        <div :id="'diaImages' + diashow.filename" class="img-fluid float-right img-thumbnail carousel slide" data-ride="carousel" data-interval="5000">
+          <div class="carousel-inner dia-image" role="listbox">
+            <img class="d-block" :src="diashow.filename" style="width: 100%;" alt="">
           </div>
         </div>
       </div>
     </div>
-    </div>
   </div>
+</div>
 </template>
 
 <script>
